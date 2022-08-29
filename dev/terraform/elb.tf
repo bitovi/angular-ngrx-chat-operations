43bf4fc1099cedd02813fdce2ed67d32e4c11f9f
Mick McGrath <mick@bitovi.com>
2022-08-29 11:50:42 -0400

    troubleshoot bucket access for lb access logs

--- dev/terraform/elb.tf
+++ dev/terraform/elb.tf
@@ -2,8 +2,32 @@ data "aws_acm_certificate" "issued" {
   domain = var.root_domain_name
 }
 
+data "aws_elb_service_account" "main" {}
 resource "aws_s3_bucket" "lb_access_logs" {
   bucket  = var.lb_access_bucket_name
+  acl    = "private"
+
+  policy = <<POLICY
+{
+  "Id": "Policy",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.lb_access_bucket_name}/*",
+      "Principal": {
+        "AWS": [
+          "${data.aws_elb_service_account.main.arn}"
+        ]
+      }
+    }
+  ]
+}
+POLICY
+
   tags = {
     Name  = var.lb_access_bucket_name
   }
